48af92aa4d1537aa66c38d1f93c3234eeb741007
Annie Huang <annie.hn@gmail.com>
2025-12-24 16:38:44 +1100

    Add the /products link into the app/index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,19 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Home page</Text>
       <Link href='/about'>About</Link>
       <Link href='/profile'>Profile</Link>
+      <Link href='/products'>Products</Link>
     </View>
   );
 }
