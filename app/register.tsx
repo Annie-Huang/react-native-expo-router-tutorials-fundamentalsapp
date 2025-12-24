96a8c1671bf695fb55a1ed383d86fe57c7f5d727
Annie Huang <annie.hn@gmail.com>
2025-12-25 10:19:36 +1100

    Create app/register.tsx file

---
+++ app/register.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+const Register = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Register</Text>
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
